40589c629db3bfc24bb9f1feaeb8abbed38b79e0
Robert John <securetorobert@protonmail.com>
2022-11-10 21:16:32 +0100

    Added Arduino MKR WiFi 1010 ENV Shield

---
+++ chapter-3/Arduino MKR WiFi 1010 ENV Shield/MyEnv.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <Arduino.h>
+#include <Arduino_JSON.h>
+
+namespace MyEnv {
+  bool initialize();
+  float getTemp();
+  float getHumid();
+  float getPressure();
+  float getLux();
+  JSONVar getJSONReadings();
+  String getReadings(); 
+}
