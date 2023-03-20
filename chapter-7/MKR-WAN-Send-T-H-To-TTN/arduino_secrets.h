f548b49d96db7d26583abdc07b49f3f39200ab5e
Robert John <securetorobert@protonmail.com>
2023-03-20 23:48:50 +0100

    Added simple sketch for making phone calls

--- chapter-7/MKR-WAN-Send-T-H-To-TTN/arduino_secrets.h
+++ chapter-7/MKR-WAN-Send-T-H-To-TTN/arduino_secrets.h
@@ -1,3 +1,3 @@
 // Replace with keys obtained from TheThingsNetwork console
-#define SECRET_APP_EUI ""
-#define SECRET_APP_KEY ""
+#define SECRET_APP_EUI "0000000000000000"
+#define SECRET_APP_KEY "460DF93D81D8766DDF6CFB4EBFCED1FB"
